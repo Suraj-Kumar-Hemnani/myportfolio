5cb1cc3a5b783247575fafd8b0ae9499421a8e71
Suraj Hemnani <surajkumar0688@gmail.com>
2022-05-09 01:03:11 +0530

    Add files via upload

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,137 @@
+import styled from "styled-components";
+
+
+export const Container = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1.8rem 10rem;
+  
+  background-color: #21212150;
+  
+  backdrop-filter: blur(6px);
+
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  z-index: 1000;
+
+  nav{
+    display: flex;
+    align-items: center;
+    gap: 1.8rem;
+    a{
+      color: #FFFF;
+      padding: 0.6rem;
+      font-family: 'Red Hat Display', sans-serif;
+      font-weight: 500;
+      text-transform: uppercase;
+      transition: filter 0.25s;
+
+      &.button{
+        padding: 0.6rem 5rem;
+      }
+
+      &:hover{
+        filter: brightness(0.6);
+      }
+    }
+
+  }
+
+  .menu-container{
+    cursor: pointer;
+    padding: 0.6rem 0;
+  }
+
+  .menu{
+    width: 2rem;
+    height: 0.2rem;
+    background: #FFFF;
+    position: relative;
+    cursor: pointer;
+    display: none;
+
+    &:before{
+      bottom: 0.5rem;
+    }
+    &:after{
+      top: 0.5rem;
+    }
+
+
+    &.active:before{
+      bottom: 0;
+      transform: rotate(45deg);
+    }
+
+    &.active:after{
+      top: 0;
+      transform: rotate(135deg);
+    }
+
+    &.active{
+      background-color: rgba(0, 0, 0, 0);
+    }
+
+  }
+
+  .menu:before, .menu:after {
+    content: "";
+    display: block;
+    position: absolute;
+    width: 100%;
+    height: 0.2rem;
+    background: #FFFF;
+    cursor: pointer;
+    transition: .6s;
+  }
+
+  .reload-logo{
+    max-width:100px;
+  }
+
+  @media only screen and (max-width: 800px) {
+    label {
+    position: relative;
+   }
+  }
+
+  @media (max-width: 960px){
+    padding: 1.8rem 3rem;
+
+    .menu{
+      display: block;
+    }
+
+    nav {
+      -ms-overflow-style: none;
+      scrollbar-width: none;
+      overflow: hidden;
+      opacity: 0;
+      visibility: hidden;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      position: fixed;
+      width: 100vw;
+      height: 100vh;
+      background: var(--blue);
+      top: 0;
+      left: 0;
+      transition: opacity 0.25s;
+      background-color: var(--theme);
+
+      a.button{
+        background-color: var(--pink);
+      }
+
+      &.active{
+        opacity: 1;
+        visibility: visible;
+      }
+    }
+  }
+  
+`
